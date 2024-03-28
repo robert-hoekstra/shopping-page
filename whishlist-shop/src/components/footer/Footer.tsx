09b6234c1f2741893c6f82bb7422973ddcdd5622
Robert Hoekstra <r.hoekstra@arcady.nl>
2024-03-28 21:28:17 +0100

    add footer

---
+++ whishlist-shop/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface FooterProps {
+	// Add any props you need for the footer here
+}
+
+const Footer: React.FC<FooterProps> = () => {
+	return (
+		<footer className="bg-gray-200 py-4">
+			{/* Add your footer content here */}
+		</footer>
+	);
+};
+
+export default Footer;
